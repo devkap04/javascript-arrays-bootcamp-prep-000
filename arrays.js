4f4871703b76f41470178ef1c314810af66c553f
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-09-10 17:35:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var addElementToBeginningOfArray = ["array"]
+var addElementToBeginningOfArray = ["array", "element"]
 addElementToBeginningOfArray.unshift("element")
